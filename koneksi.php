2ef21c60d476a35e466c38b5e56484155a2f41f7
Rizqi Kevin Octavian <kevin.octavian@delameta.com>
2025-07-31 20:28:44 +0700

    change db

--- koneksi.php
+++ koneksi.php
@@ -4,15 +4,11 @@ $username = "root";
 $password = "rootpassword";
 $dbname = "dbprostock_sql";
 
-if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
-}
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Atau menggunakan PDO
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$database", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    die("Koneksi gagal: " . $e->getMessage());
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
