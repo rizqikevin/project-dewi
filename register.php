5795c1c0922070fc4a2ae2f5d93a64b360ba6166
Rizqi Kevin Octavian <kevin.octavian@delameta.com>
2025-08-01 15:15:44 +0700

    some update

--- register.php
+++ register.php
@@ -123,7 +123,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <footer class="py-4 bg-light mt-auto">
                 <div class="container-fluid">
                     <div class="d-flex align-items-center justify-content-between small">
-                        <div class="text-muted">Copyright &copy; Gilang FR 2024</div>
+                        <div class="text-muted">Copyright &copy; Dewi N.S 2024</div>
                         <div>
                             <a href="#">Privacy Policy</a>
                             &middot;
